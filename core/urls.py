2749da91eed7e2d60cc44aa715090ea589aa15d1
Wasim Assad <waseem985asad@gmail.com>
2025-11-26 16:49:22 -0600

    create a login for teacher and student

--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 
-    # Auth
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
-    # Dashboards
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('admin-dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+
+    # quizzes
+    path('quizzes/', views.quiz_list_view, name='quiz_list'),
+    path('quizzes/<int:quiz_id>/take/', views.take_quiz_view, name='take_quiz'),
 ]
